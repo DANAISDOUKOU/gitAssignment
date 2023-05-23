f9051029a4db43006f1ebc26d2ca8cd2d5fdc690
Danai <danaisdk98@gmail.com>
2023-05-24 02:07:09 +0300

    Add sub from danai

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -5,20 +5,20 @@ public class Main {
         Scanner scanner =new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        /*int res=sum(a,b);
-        int res2=sub(a,b);
+        int res=sum(a,b);
+       /* int res2=sub(a,b);
         int res3=mul(a,b);
-        int res4 =div(a,b);
+        int res4 =div(a,b);*/
         System.out.println("result of sum :" +res);
-        System.out.println("result of sub :" +res2);
+        /*System.out.println("result of sub :" +res2);
         System.out.println("result of mul :" +res3);
         System.out.println("result of dic :" +res4);*/
     }
-    /*public static int sum(int a,int b){
+    public static int sum(int a,int b){
        int c= a+b;
        return c;
     }
-    public static int sub(int a,int b){
+   /* public static int sub(int a,int b){
         int c=b-a;
         return c;
     }
