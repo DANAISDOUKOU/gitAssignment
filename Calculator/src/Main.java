dc0215a3d6f40e2262fdda6ef7819d75a7954a4d
Danai <danaisdk98@gmail.com>
2023-05-24 02:44:51 +0300

    Add mul and div from lydia

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -6,13 +6,13 @@ public class Main {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         /*int res=sum(a,b);
-        int res2=sub(a,b);
+        int res2=sub(a,b);*/
         int res3=mul(a,b);
         int res4 =div(a,b);
-        System.out.println("result of sum :" +res);
-        System.out.println("result of sub :" +res2);
+       /* System.out.println("result of sum :" +res);
+        System.out.println("result of sub :" +res2);*/
         System.out.println("result of mul :" +res3);
-        System.out.println("result of dic :" +res4);*/
+        System.out.println("result of dic :" +res4);
     }
     /*public static int sum(int a,int b){
        int c= a+b;
@@ -22,7 +22,7 @@ public class Main {
         int c=b-a;
         return c;
     }
-
+    */
     public static int mul(int a,int b){
         int c=a*b;
         return c;
@@ -31,5 +31,5 @@ public class Main {
     public static int div(int a,int b){
         int c=a/b;
         return c;
-    }*/
+    }
 }
